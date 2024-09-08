19003d62de357b85b4070be16414cb7f4940c481
asanka3436 <asankaprabat1978@gmail.com>
2024-09-09 01:16:22 +0530

    index.js file connect to the DB

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Product = require('./models/product.model');
 const app = express();
+
+app.use(express.json());
 
 const port = 3000;
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+
+app.get('/api/products',async(req,res)=>{
+    try {
+      const product = await Product.find({});
+      res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.post('/api/products', async(req,res)=>{
+   try {
+    const product = await Product.create(req.body)
+    res.status(200).json(product);
+   } catch (error) {
+    res.status(500).json({message: error.message});
+   }
+});
 
 
 mongoose.connect("mongodb+srv://sihina:sihina2276@mongocrud.v1y9r.mongodb.net/?retryWrites=true&w=majority&appName=MongoCRUD").then(()=>{
