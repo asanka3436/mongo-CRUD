75d546f6f502ce10388d510e2f8961023f468016
asanka3436 <asankaprabat1978@gmail.com>
2024-09-09 00:38:19 +0530

    third commit

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-console.log("sihina nimnada");
-console.log("sihina nimnada");
-console.log("sihina nimnada");
-console.log("sihina nimnada");
-console.log("sihina nimnada");
-console.log("sihina nimnada");
-console.log("sihina nimnada");
-console.log("sihina nimnada");
-console.log("sihina nimnada");
-console.log("sihina nimnada");
-console.log("sihina nimnada");
-console.log("sihina nimnada");
-console.log("sihina nimnada");
-console.log("sihina nimnada");
-console.log("sihina nimnada");
-console.log("sihina nimnada");
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+const port = 3000;
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+
+
+mongoose.connect("mongodb+srv://sihina:sihina2276@mongocrud.v1y9r.mongodb.net/?retryWrites=true&w=majority&appName=MongoCRUD").then(()=>{
+    console.log("Connect to the Database....!!");
+    app.listen(port,()=>{
+        console.log(`Server is Running port ${port}`);
+    });
+}).catch(()=>{
+    console.log("Connection Fail");
+})
+
