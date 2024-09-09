5b0f01ba02914cd218d37708514609c47864eb3c
asanka3436 <asankaprabat1978@gmail.com>
2024-09-09 21:32:21 +0530

    Update and Delete API commited

--- index.js
+++ index.js
@@ -20,6 +20,20 @@ app.get('/api/products',async(req,res)=>{
     }
 });
 
+
+app.get('/api/products/:id', async (req,res)=>{
+    
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
+
 app.post('/api/products', async(req,res)=>{
    try {
     const product = await Product.create(req.body)
@@ -30,11 +44,61 @@ app.post('/api/products', async(req,res)=>{
 });
 
 
+// update API
+app.put('/api/products/:id', async (req,res)=>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id,req.body);
+
+        if(!product){
+            return res.status(404).json({})
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        
+    }
+});
+
+
+// app.put('/api/products/:id', async (req, res) => {
+//     try {
+//       const { id } = req.params;
+//       const product = await Product.findByIdAndUpdate(id, req.body, { new: true });
+  
+//       if (!product) {
+//         return res.status(404).json({ message: "Product not found" });
+//       }
+  
+//       res.status(200).json(product);
+//     } catch (error) {
+//       res.status(500).json({ message: "Server error", error });
+//     }
+//   });
+
+
+//Delete API
+
+app.delete('/api/products/:id', async (req,res)=>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+        res.status(200).json({ message: "Product deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error });
+    }
+})
+  
+
 mongoose.connect("mongodb+srv://sihina:sihina2276@mongocrud.v1y9r.mongodb.net/?retryWrites=true&w=majority&appName=MongoCRUD").then(()=>{
     console.log("Connect to the Database....!!");
     app.listen(port,()=>{
         console.log(`Server is Running port ${port}`);
     });
+
 }).catch(()=>{
     console.log("Connection Fail");
 })
